0a5f8989262d80c5d7e874de7961f63743463b28
朱国伟 <zhugw@zhuguoweideMacBook-Pro.local>
2016-06-07 20:59:01 +0800

    first commit

---
+++ src/main/java/com/example/GitTestApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTestApplication.class, args);
+	}
+}
